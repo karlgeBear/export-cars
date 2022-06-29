9decfa516c847e9aa66e729ca9daf9ca86c23c18
zengxinfu <karlge_bear@163.com>
2022-06-29 17:37:32 +0800

    Continue to dev the blog.vue

---
+++ components/BlogCard.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="blog-card">
+    <article class="blog-card__inner">
+      <a href="#">
+        <img src="~/assets/imgs/cars/blog-card-768x513.jpg" alt="blog-card-car">
+      </a>
+      <div class="blog-card__content">
+        <h3>
+            <a class="blog-card__title" href="https://chinaexportcar.com/how-close-are-we-to-autonomous-cars/" title="How Close are we to Autonomous Cars?">
+                How Close are we to Autonomous Cars?
+            </a>
+        </h3>
+        <div class="blog-card__content__top">
+            <div class="blog-card__author">
+              <div class="blog-card__author__name"><i class="far fa-user"></i>admin</div>
+            </div>
+                
+            <div class="blog-card__date">
+              <i class="far fa-calendar"></i>
+              <span>March 25, 2020</span>
+            </div>
+        </div>
+        <div class="blog-card__excerpt">
+          How the adventure ended will be seen anon. Aouda was anxious, though she said nothing. As for Passepartout, he thought Mr. Fogg’s manoeuvre simply glorious. The captain had said “between eleven and twelve knots,” and the...
+        </div>
+        <div class="blog-card-v2__button-wrapper">
+          <a class="button" href="https://chinaexportcar.com/how-close-are-we-to-autonomous-cars/" title="How Close are we to Autonomous Cars?">
+            Read more
+          </a>
+        </div>
+      </div>
+    </article>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ExportCarsBlogCard',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-card{
+  width: 50%;
+  margin: 60px 0 30px;
+  padding-right: 32px;
+  .blog-card__inner {
+    background: #FFFFFF;
+    border-radius: 10px;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    -ms-border-radius: 10px;
+    border-radius: 10px;
+    box-shadow: 1px 1px 0 0 rgba(196, 196, 196, 24%);
+    border: 1px solid #e7edf3;
+    overflow: hidden;
+    transition: all 0.2s ease;
+    position: relative;
+    &:hover{
+      border: 1px solid var(--primary);
+    }
+    >a {
+      padding-top: 0;
+      height: 300px;
+      position: relative;
+      display: block;
+      background: #f2f5fb;
+      overflow: hidden;
+      padding-top: 55.5224%;
+      border-top-right-radius: 10px;
+      border-top-left-radius: 10px;
+      color: #222732;
+      transition: all 0.3s ease;
+      text-decoration: none;
+      img{
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        position: absolute;
+        object-fit: cover;
+        opacity: 1;
+        transition: opacity 0.8s, transform 0.8s, filter 400ms;
+        border-top-right-radius: 10px;
+        border-top-left-radius: 10px;
+        &:hover{
+          transform: scale(1.11);
+        }
+      }
+    }
+    .blog-card__content {
+      padding: 30px;
+      text-align: left;
+      h3{
+        margin-bottom: 10px;
+        a{
+          font-size: 24px;
+          line-height: 32px;
+          font-weight: 700;
+          white-space: initial;
+          color: #222732;
+          display: block;
+          word-break: break-word;
+          transition: all .3s ease;
+          &:hover{
+            color: var(--primary);
+          }
+        }
+      }
+      .blog-card__content__top {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        .blog-card__author {
+          margin-bottom: 20px;
+          margin-right: 18px;
+          text-align: center;
+          display: inline-block;
+          .blog-card__author__name {
+            float: left;
+            font-size: 13px;
+            line-height: 16px;
+            font-weight: 600;
+            color: #95989d;
+            i{
+              margin-right: 4px;
+              color: var(--primary);
+            }
+          }
+        }
+       .blog-card__date {
+          font-size: 13px;
+          line-height: 16px;
+          font-weight: 600;
+          color: #95989d;
+          margin-bottom: 15px;
+          text-align: left;
+          i{
+            margin-right: 4px;
+            color: var(--primary);
+          }
+        }
+      }
+      .blog-card__excerpt {
+        margin-bottom: 20px;
+        text-align: left;
+        max-width: none;
+        margin-left: 0;
+        margin-right: 0;
+        margin: 0 0 16px 0;
+        color: #222732;
+        opacity: 0.48;
+        font-size: 15px;
+        font-weight: 500;
+        line-height: 1.64;
+      }
+      .button{
+        overflow: hidden;
+        display: inline-block;
+        font-size: 17px;
+        line-height: 21px;
+        font-weight: 600;
+        text-align: center;
+        color: #fff;
+        border: 0 solid transparent;
+        box-shadow: none;
+        cursor: pointer;
+        padding: 16px 25px 16px 25px;
+        vertical-align: top;
+        border-radius: 10px;
+        background-color: var(--primary);
+        transition: all 0.2s ease-in-out;
+        transition-property: all;
+        transition-duration: 0.2s;
+        transition-timing-function: linear;
+        transition-delay: 0s;
+        align-items: center;
+        transform: translateZ(0);
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,6 +1,81 @@
 <template>
   <div class="blog">
-    <Nav :diyrate="4" :diynavbg="1"/>
+    <Nav :diyrate="4" :diynavbg="0"/>
+    <div class="blog-header">
+      <div class="elementor-background-overlay"></div>
+      <div class="elementor-titles">
+        <h1>Our Latest <span class="highColor">News</span></h1>
+        <h2>From spy shots to new releases to auto show coverage</h2>
+      </div>
+    </div>
+    <div class="blog-bread-nav"><BreadNav /></div>
+    <div class="blog-main container">
+      <div class="blog-main-left">
+        <BlogCard />
+        <BlogCard />
+      </div>
+      <div class="blog-main-right">
+        
+        <div class="blog-search">
+          <el-input placeholder="Search ..." suffix-icon="el-icon-search" v-model="serachInput"></el-input>
+        </div>
+        <div class="categories">
+          <h4>Recent Posts</h4>
+          <ul class="categories-list">
+            <li class="category"><a href="#">Auto Detailing</a>(1)</li>
+            <li class="category"><a href="#">Car News</a>(2)</li>
+            <li class="category"><a href="#">Car Reviews</a>(3)</li>
+          </ul>
+        </div>
+        <div class="recent-posts">
+          <h4>Recent Posts</h4>
+          <div class="recent-post">
+            <div class="recent-post__img">
+              <img src="~/assets/imgs/cars/blog-card-768x513.jpg" alt="blog-card-car">
+            </div>
+            <div class="recent-post__content">
+              <a href="#">How Close are we to Autonomous Cars?</a>
+              <a href="#">Read more</a>
+            </div>
+          </div>
+          <div class="recent-post">
+           <div class="recent-post__img">
+            <img src="~/assets/imgs/cars/blog-card-768x513.jpg" alt="blog-card-car">
+           </div>
+            <div class="recent-post__content">
+              <a href="#">How Close are we to Autonomous Cars?</a>
+              <a href="#">Read more</a>
+            </div>
+          </div>
+          <div class="post-link text-clik">Read All Posts</div>
+        </div> 
+        <div class="about-us">
+          <h4>About Us</h4>
+          <div class="about-us__content">
+            <div class="about-us-person">
+              <div class="about-us-person__avatar">
+                <img src="" alt="">
+              </div>
+              <div class="about-us-person__info">
+                <div class="about-us-person__name">John Hendricks</div>
+                <div class="about-us-person__intro">Blog Editor</div>
+              </div>
+            </div>
+            <p class="about-us-person__desc">
+              We went down the lane, by the body of the man in black, sodden now from the overnight hail, and broke into the woods..
+            </p>
+          </div>
+        </div>
+        <div class="popular-tags">
+          <a href="">premium</a>
+          <a href="">sedan</a>
+          <a href="">speed</a>
+          <a href="">sport</a>
+          <a href="">supercars</a>
+          <a href="">suv</a>
+        </div>
+      </div>
+    </div>
     <h1>BLOG</h1>
     <Footer />
   </div>
@@ -12,7 +87,7 @@ export default {
 
   data() {
     return {
-      
+      serachInput: ''
     };
   },
 
@@ -27,5 +102,99 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.blog-header{
+    height: 500px;
+    background-image: url(https://chinaexportcar.com/wp-content/uploads/2020/09/photo-hoteam-196xdwXDHc0-unsplash.jpg);
+    background-position: center center;
+    background-size: cover;
+    margin-top: -105px;
+    padding-top: 130px;
+    position: relative;
+    z-index: 1;
+    .elementor-background-overlay{
+      background-color: #0F141E;
+      opacity: 0.6;
+      height: 100%;
+      width: 100%;
+      top: 0;
+      left: 0;
+      position: absolute;
+      transition: background 0.3s, border-radius 0.3s, opacity 0.3s;
+      z-index: -1;
+    }
+    .elementor-titles{
+      text-align: center;
+      height: 100%;
+      padding: 80px 0;
+      h1{
+        color: #FFFFFF;
+        font-size: 45px;
+        line-height: 51px;
+        font-weight: 900;
+      }
+      h2{
+        color: #FFFFFF9C;
+        font-size: 22px;
+        font-weight: 500;
+        line-height: 51px;
+      }
+    }
+}
+.blog-bread-nav{
+  background-color: var(--bg);
+}
+.blog-main{
+  display: flex;
+  .blog-main-left{
+    width: 70%;
+    display: flex;
+  }
+  .blog-main-right{
+    margin-top: 60px;
+    width: 30%;
+    >div{
+      margin-bottom: 40px;
+    }
+    .blog-search{
+      padding: 20px 20px;
+      background-color: var(--bg);
+      border-radius: 10px;
+    }
+    h4{
+      font-size: 21px;
+      line-height: 30px;
+      font-weight: 600;
+      margin-bottom: 20px;
+    }
+    .categories{
+      .categories-list{
+        padding: 20px 30px;
+        box-shadow: 1px 1px 0 0 rgba(196, 196, 196, 24%);
+        border: solid 1px #e7edf3;
+        border-radius: 10px;
+        li:not(:last-child){
+          padding-bottom: 15px;
+          border-bottom: 1px solid var(--bg);
+          margin-bottom: 15px;
+          text-align: right;
+          color: var(--primary);
+          a{
+            float: left;
+            color: #222732;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
 
+<style lang="scss">
+.blog{
+  .el-input__inner{
+    height:50px;
+    border-radius: 10px;
+    font-weight: 600;
+  }
+}
 </style>
