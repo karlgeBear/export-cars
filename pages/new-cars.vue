a5865f342694590f212a41f9611f9693def1f409
zengxinfu <karlge_bear@163.com>
2022-07-08 17:35:20 +0800

    The basic new-car.vue has been developed

--- pages/new-cars.vue
+++ pages/new-cars.vue
@@ -34,6 +34,55 @@
         </CommonSearchBg>
       </div>
     </div>
+    <div class="all-car-make">
+      <div class="container">
+        <div class="all-car-make__tab-hover">
+          <div class="all-car-make__title">
+            <div class="title-widget highColor">AUTO SINA</div>
+            <h1 class="title">新能源品牌</h1>
+          </div>
+          <div class="tab">
+            <ul class="tab-hover-list">
+              <li class="tab-hover-item" v-for="(item,index) in makeFilterOption" :key="index"
+              :class="{active:isActive==index}"
+              @mouseenter='hover(index)'
+              >{{Object.keys(item)[0]}}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="all-car-make__tab-show">
+          <div class="car-make__single-box" v-for="(item,index) in makeFilterOption" :key="index" v-show="isActive == index">
+            <ul class="tab-show-list">
+              <li class="tab-show-item" v-for="(itemChild,indexChild) in Object.values(item)[0]" :key="indexChild">
+                <a href="#" target="_blank" class="img" :alt="itemChild">
+                  <img src="http://n.sinaimg.cn/auto/transform/brand/190/w95h95/20210722/25dd-3e9e30425bdafa619dbda61343da85a2.jpg" alt="捷豹">
+                  <div class="carname">{{itemChild}}</div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="long-battery-life">
+      <div class="container">
+        <div class="long-battery-life__title">
+          <div class="title-widget highColor">AUTO SINA</div>
+          <h1 class="title">超长续航</h1>
+        </div>
+      </div>
+      <CommonCarList :carListData="carListData" cardW="18%"></CommonCarList>
+    </div>
+    <div class="bottom-search container">
+      <input type="text" placeholder="输入你的关键词">
+      <button>
+        <i data-v-e2da6a34="" class="fa-search"></i>
+      </button>
+      <a href="#" class="arrow-back-top">
+        <img src="~/assets/imgs/icon/arrow-back-top.png" alt="arrow-back-top"/>
+      </a>
+    </div>
     <Footer />
   </div>
 </template>
@@ -46,6 +95,7 @@ export default {
     return {
       value: "",
       serachKeyValue:'',
+      isActive: 0,
        options: [
         {
           value: "1",
@@ -68,6 +118,113 @@ export default {
           label: "Cadillac",
         },
       ],
+      makeFilterOption: [
+        {
+          '热门品牌':[
+          '奔驰','宝马','奥迪','别克','丰田','沃尔沃','长安汽车','荣威',
+          '奔驰','宝马','奥迪','别克','丰田','沃尔沃','长安汽车','荣威'
+          ]
+        },
+        {'A': ['Audi']},
+        {'B': ['BWN']},
+        {'C':['Cupra']},
+        {'D':['DMC']},
+        {'E':['need fill carData']},
+        {'F':['need fill carData']},
+        {'G':['need fill carData']},
+        {'H':['need fill carData']},
+        {'I':['need fill carData']},
+        {'J':['need fill carData']},
+        {'K':['need fill carData']},
+        {'L':['need fill carData']},
+        {'M':['need fill carData']},
+        {'N':['need fill carData']},
+        {'O':['need fill carData']},
+        {'P':['need fill carData']},
+        {'Q':['need fill carData']},
+        {'R':['need fill carData']},
+        {'S':['need fill carData']},
+        {'T':['need fill carData']},
+        {'V':['need fill carData']},
+        {'W':['need fill carData']},
+        {'X':['need fill carData']},
+        {'Y':['need fill carData']},
+        {'Z':['need fill carData']},
+      ],
+      carListData: [
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-1.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-2.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-3.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-4.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-1.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-2.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-3.jpg'),
+          pictureSum: 6
+        },
+        {
+          title:'Ferrari LaFerrari 2-door coupe red',
+          price:'810,000',
+          year:'2021',
+          Transmission:'Auto',
+          Kilometers: 17882,
+          pictures:require('~/assets/imgs/cars/swiper-car-4.jpg'),
+          pictureSum: 6
+        }
+      ]
     };
   },
 
@@ -76,12 +233,42 @@ export default {
   },
 
   methods: {
-    
+    hover(val){
+      this.isActive = val
+      console.log(val)
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.active{
+  color: #FF4605;
+  font-weight: bold;
+  &::after{
+    content:'';
+    width: 100%;
+    height: 4px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background: #FF4605;
+    border-radius: 200px 200px 200px 200px;
+    opacity: 1;
+  }
+}
+.title-widget{
+  width: 222px;
+  height: 40px;
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 3px;
+  +.title{
+    width: 379px;
+    font-size: 60px;
+    letter-spacing: 3px;
+  }
+}
 .search{
   background-image: url(~assets/imgs/bg-new-car.jpg);
   height: 620px;
@@ -117,6 +304,93 @@ export default {
     border: 0;
     height: 100%;
     letter-spacing: 2px;
+  }
+}
+.all-car-make{
+  margin: 50px 0;
+  .all-car-make__tab-hover{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom:55px;
+    .tab{
+      width: 100%;
+      align-self: flex-end;
+      .tab-hover-list{
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        .tab-hover-item{
+          position: relative;
+          padding-bottom: 9px;
+          display: inline-block;
+          cursor: pointer;
+        }
+      }
+    }
+
+  }
+  .all-car-make__tab-show{
+    .car-make__single-box{
+      .tab-show-list{
+        display: flex;
+        flex-wrap: wrap;
+        li{
+          width: 10%;
+          text-align: center;
+          a{
+            display: inline-block;
+            width: 100%;
+            img{
+              width: 100%;
+              padding:10px;
+            }
+            .carname{
+              padding-top: 10px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+.long-battery-life{
+  .long-battery-life__title{
+    padding-bottom: 90px;
+  }
+  background-color: #EFF3FA;
+  padding: 120px 250px;
+}
+.bottom-search{
+  padding: 68px 0 88px;
+  text-align: center;
+  position: relative;
+  input{
+    border: 1px solid #999999;
+    width: 350px;
+    height: 60px;
+    padding-left: 20px;
+    background: #FFFFFF;
+    border-radius:20px;
+    opacity: 1;
+    border: 1px solid #999999;
+  }
+  button{
+    width: 57px;
+    height: 53px;
+    background-color: var(--primary);
+    border-radius: 10px;
+    i{
+      font-family: "Font Awesome 5 Free";
+      font-weight: 600;
+      color: #ffffff;
+    }
+  }
+  a{
+    display: inline-block;
+    width: 57px;
+    height: 53px;
+    position: absolute;
+    right: 0;
   }
 }
 </style>
